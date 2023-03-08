e75aa49ea8df326263ba62ce96073ea5277ae37d
Yossi Bokor Bleile <yossi@yossi.eu>
2023-03-08 22:37:55 +0100

    interim commit

--- correa-test.py
+++ correa-test.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.append("/Users/yossi/correa_private/src/")
+
 import correa
-p = correa.PyPolygon('/Users/yossi/AAU/ToMaCo/contours/contours/01kPa/24h_Trypsin24h_2-FITC_001_png_contour.csv')
+p = correa.create_polygon('/Users/yossi/AAU/ToMaCo/contours/contours/01kPa/24h_Trypsin24h_2-FITC_001_png_contour.csv')
 p.vertices()
 p.ellipse_min()
 p.ellipse_max()
